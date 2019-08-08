d7dd07ac294ca8a845a8444475f75845be27fda7
thang1160 <thang1160@users.noreply.github.com>
2019-08-08 18:26:41 +0700

    đổi layout

--- Web_App/Controllers/CDVT/CDVTController.cs
+++ Web_App/Controllers/CDVT/CDVTController.cs
@@ -14,22 +14,12 @@ namespace Web_App.Controllers
             return View();
         }
 
-        public ActionResult Chi_tiet_su_co()
-        {
-            return View();
-        }
-
         public ActionResult Lich_su_chung_su_co()
         {
             return View();
         }
 
         public ActionResult Theo_doi_bao_hanh()
-        {
-            return View();
-        }
-
-        public ActionResult Theo_doi_bao_hiem_ky_han()
         {
             return View();
         }
@@ -44,12 +34,58 @@ namespace Web_App.Controllers
             return View();
         }
 
-        public ActionResult Theo_doi_kiem_dinh()
+        public ActionResult Theo_doi_nhien_lieu()
         {
             return View();
         }
 
-        public ActionResult Theo_doi_nhien_lieu()
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Theo_doi_chung_oto()
+        {
+            return View();
+        }
+
+
+        public ActionResult Danh_sach_chung()
+        {
+            return View();
+        }
+
+        public ActionResult Chi_tiet_thiet_bi_may()
+        {
+            return View();
+        }
+
+        public ActionResult Bao_duong_thiet_bi()
+        {
+            return View();
+        }
+
+        public ActionResult Chi_tiet_o_to()
+        {
+            return View();
+        }
+
+        public ActionResult Bao_duong_o_to()
+        {
+            return View();
+        }
+
+        public ActionResult Chi_tiet_sua_chua()
+        {
+            return View();
+        }
+
+        public ActionResult Chi_tiet_su_co()
+        {
+            return View();
+        }
+
+        public ActionResult Theo_doi_kiem_dinh()
         {
             return View();
         }
@@ -59,7 +95,7 @@ namespace Web_App.Controllers
             return View();
         }
 
-        public ActionResult Index()
+        public ActionResult Theo_doi_bao_hiem_ky_han()
         {
             return View();
         }
