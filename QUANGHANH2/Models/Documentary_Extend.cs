7e39d0702f63a4b666e4c92456aee8600b2b43ef
DungNXH <55150405+DungNXH@users.noreply.github.com>
2019-09-20 11:15:05 +0700

    Change Nghiem Thu

--- QUANGHANH2/Models/Documentary_Extend.cs
+++ QUANGHANH2/Models/Documentary_Extend.cs
@@ -8,5 +8,8 @@ namespace QUANGHANH2.Models
     public class Documentary_Extend : Documentary
     {
         public string templdId { get; set; }
+        public string equipmentId { get; set; }
+        public string equipment_name { get; set; }
+        public System.DateTime acceptance_date { get; set; }
     }
 }
