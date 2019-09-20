6ec8b7a69b107cd64939776c33ad78212fcee9c4
sonnh <yggdrasil2769@gmail.com>
2019-09-20 14:58:45 +0700

    Revert "Update DB from SONNH"
    This reverts commit 85b8d42ad5754e2eecd42f3f2ff1ff737a4b694c.

---
+++ QUANGHANH2/Models/ChungChi_NhanVien_Model.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace QUANGHANH2.Models
+{
+    public class ChungChi_NhanVien_Model
+    {
+        [Required(ErrorMessage = "Không để trống")]
+        public string SoHieu { get; set; }
+        [Required(ErrorMessage = "Không để trống")]
+        public Nullable<System.DateTime> NgayCap { get; set; }
+        [Required(ErrorMessage = "Không để trống")]
+        public string MaNV { get; set; }
+        [Required(ErrorMessage = "Không để trống")]
+        public int MaChungChi { get; set; }
+        public string TenNV { get; set; }
+        public string TenChungChi { get; set; }
+
+    }
+}
