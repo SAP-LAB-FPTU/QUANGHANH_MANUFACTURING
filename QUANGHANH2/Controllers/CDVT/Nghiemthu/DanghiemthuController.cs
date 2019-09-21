9505880cb247d81e00d42f73536fe7b0047e0a32
DungNXH <55150405+DungNXH@users.noreply.github.com>
2019-09-21 15:38:23 +0700

    abc

--- QUANGHANH2/Controllers/CDVT/Nghiemthu/DanghiemthuController.cs
+++ QUANGHANH2/Controllers/CDVT/Nghiemthu/DanghiemthuController.cs
@@ -8,6 +8,7 @@ using System.Web.Routing;
 using System.Linq.Dynamic;
 using System.Data.Entity;
 using QUANGHANH2.SupportClass;
+using System.Globalization;
 
 namespace QUANGHANHCORE.Controllers.CDVT.Nghiemthu
 {
@@ -24,7 +25,7 @@ namespace QUANGHANHCORE.Controllers.CDVT.Nghiemthu
         public ActionResult GetData()
         {
             //Server Side Parameter
-            string requestID = Request["sessionId"];
+            //string requestID = Request["sessionId"];
             int start = Convert.ToInt32(Request["start"]);
             int length = Convert.ToInt32(Request["length"]);
             string searchValue = Request["search[value]"];
@@ -59,7 +60,59 @@ namespace QUANGHANHCORE.Controllers.CDVT.Nghiemthu
                 int totalrows = docList.Count;
                 int totalrowsafterfiltering = docList.Count;
                 //sorting
-                //docList = docList.OrderBy(sortColumnName + " " + sortDirection).ToList<Documentary>();
+                docList = docList.OrderBy(sortColumnName + " " + sortDirection).ToList<Documentary_Extend>();
+                //paging
+                docList = docList.Skip(start).Take(length).ToList<Documentary_Extend>();
+                return Json(new { success = true, data = docList, draw = Request["draw"], recordsTotal = totalrows, recordsFiltered = totalrowsafterfiltering }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        [Route("phong-cdvt/da-nghiem-thu/search")]
+        [HttpPost]
+        public ActionResult Search(string equimentid,string equimentname, string date_start, string date_end)
+        {
+            //Server Side Parameter
+            string requestID = Request["sessionId"];
+            int start = Convert.ToInt32(Request["start"]);
+            int length = Convert.ToInt32(Request["length"]);
+            string searchValue = Request["search[value]"];
+            string sortColumnName = Request["columns[" + Request["order[0][column]"] + "][name]"];
+            string sortDirection = Request["order[0][dir]"];
+            List<Documentary_Extend> docList = new List<Documentary_Extend>();
+
+            DateTime dstart = DateTime.ParseExact(date_start, "dd/MM/yyyy", CultureInfo.InvariantCulture);
+            DateTime dend = DateTime.ParseExact(date_end, "dd/MM/yyyy", CultureInfo.InvariantCulture);
+            //
+            using (QUANGHANHABCEntities db = new QUANGHANHABCEntities())
+            {
+                db.Configuration.LazyLoadingEnabled = false;
+                if(!equimentid.Equals("") || !equimentname.Equals("") || !dstart.Equals("") || !dend.Equals(""))
+                docList = (from a in db.Acceptances
+                          
+                           join b in db.Equipments on a.equipmentId equals b.equipmentId
+                           where (a.equipmentStatus == 3) && (a.equipmentId.Contains(equimentid) && b.equipment_name.Contains(equimentname) && (a.acceptance_date >= dstart && a.acceptance_date <= dend))
+                           select new
+                           {
+                               //documentary_id = a.documentary_id,
+                               equipmentId = b.equipmentId,
+                               equipment_name = b.equipment_name,
+                               acceptance_date = a.acceptance_date
+
+
+                           }).ToList().Select(p => new Documentary_Extend
+                           {
+                               //documentary_id = p.documentary_id,
+                               equipmentId = p.equipmentId,
+                               equipment_name = p.equipment_name,
+                               acceptance_date = p.acceptance_date
+
+                           }).ToList();
+
+                //docList = db.Documentaries.ToList<Documentary>();
+                int totalrows = docList.Count;
+                int totalrowsafterfiltering = docList.Count;
+                //sorting
+                docList = docList.OrderBy(sortColumnName + " " + sortDirection).ToList<Documentary_Extend>();
                 //paging
                 docList = docList.Skip(start).Take(length).ToList<Documentary_Extend>();
                 return Json(new { success = true, data = docList, draw = Request["draw"], recordsTotal = totalrows, recordsFiltered = totalrowsafterfiltering }, JsonRequestBehavior.AllowGet);
