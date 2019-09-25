8da730310d773028638c549032ed5968759d7153
DungNXH <55150405+DungNXH@users.noreply.github.com>
2019-09-25 09:45:20 +0700

    Update

--- QUANGHANH2/Controllers/CDVT/Quyetdinh/ChitietKiemdinhController.cs
+++ QUANGHANH2/Controllers/CDVT/Quyetdinh/ChitietKiemdinhController.cs
@@ -20,90 +20,90 @@ namespace QUANGHANHCORE.Controllers.CDVT.Quyetdinh
             return View("/Views/CDVT/Quyet_dinh/Chi_tiet_kiem_dinh.cshtml");
         }
 
-        [HttpPost]
-        public ActionResult Detail()
+        //    [HttpPost]
+        //    public ActionResult Detail()
+        //    {
+        //        string requestID = Request["sessionId"];
+        //        int start = Convert.ToInt32(Request["start"]);
+        //        int length = 10;
+        //        string searchValue = Request["search[value]"];
+        //        string sortColumnName = Request["columns[" + Request["order[0][column]"] + "][name]"];
+        //        string sortDirection = Request["order[0][dir]"];
+
+        //        using (QUANGHANHABCEntities db = new QUANGHANHABCEntities())
+        //        {
+        //            //db.Configuration.LazyLoadingEnabled = false;
+        //            //int count = 1;
+        //            //List<NewDocumentary_repair_details> documentariesList = (from a in db.Documentary_repair_details
+        //            //                                                      where (a.documentary_id == requestID)
+        //            //                                                      join b in db.Documentaries on a.documentary_id equals b.documentary_id
+        //            //                                                      join c in db.Equipments on a.equipmentId equals c.equipmentId
+        //            //                                                      join d in db.Departments on c.department_id equals d.department_id
+        //            //                                                      select new
+        //            //                                                      {
+        //            //                                                          documentary_id = a.documentary_id,
+        //            //                                                          equipment_name = c.equipment_name,
+        //            //                                                          department_name = d.department_name,
+        //            //                                                          department_id = c.department_id,
+        //            //                                                          repair_reason = a.repair_reason,
+        //            //                                                          repair_type = a.repair_type,
+        //            //                                                          reason = b.reason,
+        //            //                                                          finish_date_plan = a.finish_date_plan
+        //            //                                                      }).ToList().Select(p => new NewDocumentary_repair_details
+        //            //                                                      {
+        //            //                                                          documentary_id = p.documentary_id,
+        //            //                                                          equipment_name = p.equipment_name,
+        //            //                                                          department_name = p.department_name,
+        //            //                                                          department_id = p.department_id,
+        //            //                                                          repair_reason = p.repair_reason,
+        //            //                                                          repair_type = p.repair_type,
+        //            //                                                          reason = p.reason,
+        //            //                                                          finish_date_plan = p.finish_date_plan
+        //            //                                                      }).ToList();
+        //            //foreach (var el in documentariesList)
+        //            //{
+        //            //    el.order_number = count++;
+
+        //            //}
+        //            //int totalrows = documentariesList.Count;
+        //            //int totalrowsafterfiltering = documentariesList.Count;
+
+        //            ////sorting
+        //            //documentariesList = documentariesList.OrderBy(sortColumnName + " " + sortDirection).ToList<NewDocumentary_repair_details>();
+        //            ////paging
+
+        //            //documentariesList = documentariesList.Skip(start).Take(length).ToList<NewDocumentary_repair_details>();
+        //            //Console.WriteLine(Json(new { success = true, data = documentariesList, draw = Request["draw"], recordsTotal = totalrows, recordsFiltered = totalrowsafterfiltering }, JsonRequestBehavior.AllowGet));
+
+        //            //var js = Json(new { success = true, data = documentariesList, draw = Request["draw"], recordsTotal = totalrows, recordsFiltered = totalrowsafterfiltering }, JsonRequestBehavior.AllowGet);
+
+        //            //var result = new JavaScriptSerializer().Serialize(js.Data);
+        //            //ViewBag.count = 0;
+        //            //return js;
+        //        }
+        //    }
+        //}
+        public class NewDocumentary_repair_details : Documentary_repair_details
         {
-            string requestID = Request["sessionId"];
-            int start = Convert.ToInt32(Request["start"]);
-            int length = 10;
-            string searchValue = Request["search[value]"];
-            string sortColumnName = Request["columns[" + Request["order[0][column]"] + "][name]"];
-            string sortDirection = Request["order[0][dir]"];
+            public string repair_type { get; set; }
 
-            using (QUANGHANHABCEntities db = new QUANGHANHABCEntities())
-            {
-                db.Configuration.LazyLoadingEnabled = false;
-                int count = 1;
-                List<NewDocumentary_repair_details> documentariesList = (from a in db.Documentary_repair_details
-                                                                      where (a.documentary_id == requestID)
-                                                                      join b in db.Documentaries on a.documentary_id equals b.documentary_id
-                                                                      join c in db.Equipments on a.equipmentId equals c.equipmentId
-                                                                      join d in db.Departments on c.department_id equals d.department_id
-                                                                      select new
-                                                                      {
-                                                                          documentary_id = a.documentary_id,
-                                                                          equipment_name = c.equipment_name,
-                                                                          department_name = d.department_name,
-                                                                          department_id = c.department_id,
-                                                                          repair_reason = a.repair_reason,
-                                                                          repair_type = a.repair_type,
-                                                                          reason = b.reason,
-                                                                          finish_date_plan = a.finish_date_plan
-                                                                      }).ToList().Select(p => new NewDocumentary_repair_details
-                                                                      {
-                                                                          documentary_id = p.documentary_id,
-                                                                          equipment_name = p.equipment_name,
-                                                                          department_name = p.department_name,
-                                                                          department_id = p.department_id,
-                                                                          repair_reason = p.repair_reason,
-                                                                          repair_type = p.repair_type,
-                                                                          reason = p.reason,
-                                                                          finish_date_plan = p.finish_date_plan
-                                                                      }).ToList();
-                foreach (var el in documentariesList)
-                {
-                    el.order_number = count++;
+            public string repair_reason { get; set; }
 
-                }
-                int totalrows = documentariesList.Count;
-                int totalrowsafterfiltering = documentariesList.Count;
+            public System.DateTime finish_date_plan { get; set; }
 
-                //sorting
-                documentariesList = documentariesList.OrderBy(sortColumnName + " " + sortDirection).ToList<NewDocumentary_repair_details>();
-                //paging
+            public string documentary_id { get; set; }
 
-                documentariesList = documentariesList.Skip(start).Take(length).ToList<NewDocumentary_repair_details>();
-                Console.WriteLine(Json(new { success = true, data = documentariesList, draw = Request["draw"], recordsTotal = totalrows, recordsFiltered = totalrowsafterfiltering }, JsonRequestBehavior.AllowGet));
+            public string equipmentId { get; set; }
 
-                var js = Json(new { success = true, data = documentariesList, draw = Request["draw"], recordsTotal = totalrows, recordsFiltered = totalrowsafterfiltering }, JsonRequestBehavior.AllowGet);
+            public string department_id { get; set; }
 
-                var result = new JavaScriptSerializer().Serialize(js.Data);
-                ViewBag.count = 0;
-                return js;
-            }
+            public string equipment_name { get; set; }
+
+            public string department_name { get; set; }
+
+            public string reason { get; set; }
+
+            public int order_number { get; set; }
         }
     }
-    public class NewDocumentary_repair_details : Documentary_repair_details
-    {
-        public string repair_type { get; set; }
-
-        public string repair_reason { get; set; }
-
-        public System.DateTime finish_date_plan { get; set; }
-
-        public string documentary_id { get; set; }
-
-        public string equipmentId { get; set; }
-
-        public string department_id { get; set; }
-
-        public string equipment_name { get; set; }
-    
-        public string department_name { get; set; }
-  
-        public string reason { get; set; }
-
-        public int order_number { get; set; }
-    }
-
 }
