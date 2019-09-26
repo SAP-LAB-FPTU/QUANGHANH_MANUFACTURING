fc4dcec87ccba9e81f414072f86c30a122c14233
huong97 <nguyenhuong3397@gmail.com>
2019-09-26 14:01:21 +0700

    Sửa và thêm model
    Sửa model ChungChi_NhanVien_Model và thêm model ChungChi_detailsDB

--- QUANGHANH2/Models/ChungChi_NhanVien_Model.cs
+++ QUANGHANH2/Models/ChungChi_NhanVien_Model.cs
@@ -8,6 +8,14 @@ namespace QUANGHANH2.Models
 {
     public class ChungChi_NhanVien_Model:ChungChi_NhanVien
     {
+        [Required(ErrorMessage = "Không được để trống")]
+        new public string SoHieu { get; set; }
+        [Required(ErrorMessage = "Không được để trống")]
+        new public Nullable<System.DateTime> NgayCap { get; set; }
+        [Required(ErrorMessage = "Không được để trống")]
+        new public string MaNV { get; set; }
+        [Required(ErrorMessage = "Không được để trống")]
+        new public int MaChungChi { get; set; }
         public string TenNV { get; set; }
         public string TenChungChi { get; set; }
 
