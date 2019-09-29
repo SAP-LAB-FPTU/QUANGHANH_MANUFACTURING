a4c4becc2bc32016d7065633a6420d731010d604
thang1160 <thang1160@users.noreply.github.com>
2019-09-30 02:41:16 +0700

    sửa lỗi sự cố và xử lý quyết định

--- QUANGHANH2/Controllers/CDVT/Cap_nhat/Chitiet/QDThuhoiController.cs
+++ QUANGHANH2/Controllers/CDVT/Cap_nhat/Chitiet/QDThuhoiController.cs
@@ -23,6 +23,8 @@ namespace QUANGHANH2.Controllers.CDVT.Cap_nhat
                 QUANGHANHABCEntities DBContext = new QUANGHANHABCEntities();
                 Documentary documentary = DBContext.Database.SqlQuery<Documentary>("SELECT docu.*, docu.[out/in_come] as out_in_come FROM Documentary_revoke_details as detail inner join Documentary as docu on detail.documentary_id = docu.documentary_id WHERE docu.documentary_code IS NOT NULL AND detail.documentary_id = @documentary_id",
                     new SqlParameter("documentary_id", id)).First();
+                if (documentary.documentary_status == 0) ViewBag.AddAble = true;
+                else ViewBag.AddAble = false;
                 ViewBag.id = documentary.documentary_id;
                 ViewBag.code = documentary.documentary_code as string;
                 return View("/Views/CDVT/Cap_nhat/Chitiet/Thuhoi.cshtml");
@@ -84,6 +86,14 @@ namespace QUANGHANH2.Controllers.CDVT.Cap_nhat
                         {
                             Documentary_revoke_details temp = DBContext.Documentary_revoke_details.Find(idnumber, item);
                             temp.equipment_revoke_status = 1;
+                            Acceptance a = new Acceptance();
+                            a.acceptance_date = DateTime.Now;
+                            a.acceptance_result = null;
+                            a.documentary_id = idnumber;
+                            a.documentary_process_result = null;
+                            a.equipmentId = item;
+                            a.equipmentStatus = 2;
+                            DBContext.Acceptances.Add(a);
                             DBContext.SaveChanges();
                         }
                         if (DBContext.Database.SqlQuery<Documentary_revoke_detailsDB>("select details.equipment_revoke_status from Department depa inner join Documentary docu on depa.department_id = docu.department_id inner join Documentary_revoke_details details on details.documentary_id = docu.documentary_id inner join Equipment e on e.equipmentId = details.equipmentId where docu.documentary_type = 4 and details.documentary_id = @documentary_id and equipment_revoke_status = '0'", new SqlParameter("documentary_id", id)).Count() == 0)
