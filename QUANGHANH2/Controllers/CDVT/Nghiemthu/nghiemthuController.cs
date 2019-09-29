885d90cd421994c2086bed6c476907c2a930e129
DungNXH <55150405+DungNXH@users.noreply.github.com>
2019-09-29 21:08:38 +0700

    Sửa ngôn ngữ phân trang
    Chuyển ngôn ngữ phân trang sang tiếng việt

--- QUANGHANH2/Controllers/CDVT/Nghiemthu/nghiemthuController.cs
+++ QUANGHANH2/Controllers/CDVT/Nghiemthu/nghiemthuController.cs
@@ -20,52 +20,8 @@ namespace QUANGHANHCORE.Controllers.CDVT.Nghiemthu
         {
             return View("/Views/CDVT/Nghiemthu/Nghiemthu.cshtml");
         }
-        //[Route("phong-cdvt/nghiem-thu")]
-        //[HttpPost]
-        //public ActionResult GetData()
-        //{
-        //    //Server Side Parameter
-        //    //string requestID = Request["sessionId"];
-        //    int start = Convert.ToInt32(Request["start"]);
-        //    int length = Convert.ToInt32(Request["length"]);
-        //    string searchValue = Request["search[value]"];
-        //    string sortColumnName = Request["columns[" + Request["order[0][column]"] + "][name]"];
-        //    string sortDirection = Request["order[0][dir]"];
-        //    List<Documentary_Extend> docList = new List<Documentary_Extend>();
-        //    //
-        //    using (QUANGHANHABCEntities db = new QUANGHANHABCEntities())
-        //    {
 
-        //        db.Configuration.LazyLoadingEnabled = false;
-        //        docList = (from a in db.Acceptances
-        //                   where (a.equipmentStatus == 2)
-        //                   join b in db.Equipments on a.equipmentId equals b.equipmentId
-        //                   select new
-        //                   {
-        //                       documentary_id = a.documentary_id,
-        //                       equipmentId = b.equipmentId,
-        //                       equipment_name = b.equipment_name
-
-        //                   }).ToList().Select(p => new Documentary_Extend
-        //                   {
-        //                       documentary_id = p.documentary_id,
-        //                       equipmentId = p.equipmentId,
-        //                       equipment_name = p.equipment_name
-        //                   }).ToList();
-
-        //        //docList = db.Documentaries.ToList<Documentary>();
-        //        int totalrows = docList.Count;
-        //        int totalrowsafterfiltering = docList.Count;
-
-        //        //sorting
-        //        docList = docList.OrderBy(sortColumnName + " " + sortDirection).ToList<Documentary_Extend>();
-
-        //        //paging
-        //        docList = docList.Skip(start).Take(length).ToList<Documentary_Extend>();
-        //        return Json(new { success = true, data = docList, draw = Request["draw"], recordsTotal = totalrows, recordsFiltered = totalrowsafterfiltering }, JsonRequestBehavior.AllowGet);
-        //    }
-        //}
-
+        [Auther(RightID = "25")]
         [Route("phong-cdvt/nghiem-thu/search")]
         [HttpPost]
         public ActionResult Search(string document_code, string equiment_id, string equiment_name)
@@ -171,9 +127,16 @@ namespace QUANGHANHCORE.Controllers.CDVT.Nghiemthu
         {
             using (QUANGHANHABCEntities db = new QUANGHANHABCEntities())
             {
-                var query = "UPDATE Documentary SET documentary_status = 3 FROM Acceptance T1, Documentary T2 WHERE T1.documentary_id = T2.documentary_id AND T1.equipmentId = '" + id + "'";
-                db.Database.ExecuteSqlCommand(query);
-                db.SaveChanges();
+                try
+                {
+                    var query = "UPDATE Documentary SET documentary_status = 3 FROM Acceptance T1, Documentary T2 WHERE T1.documentary_id = T2.documentary_id AND T1.equipmentId = '" + id + "'";
+                    db.Database.ExecuteSqlCommand(query);
+                    db.SaveChanges();
+                }
+                catch
+                {
+
+                }
                 return RedirectToAction("Search");
             }
         }
