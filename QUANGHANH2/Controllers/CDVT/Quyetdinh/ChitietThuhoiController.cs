64c6ccaea0f0d1084d31da3f498e618511fafe58
Trung <trungtdse05083@fpt.edu.vn>
2019-09-29 15:07:47 +0700

    Thêm controller ProcessDetail, Sửa Các chi tiết quyết định, Thêm tiếng việt vào dataTable
    - Sửa truyền id, thêm 1 số trường vào linq của function Detail trong các controller: ChitietBaoduowng, ChitietQuyetDinh, chitietsctx, ChitietSuachua, ChitietThanhli, ChitietThuhoi
    - Thêm ProcessDetailController
    - Sửa 1 số cột trong DataTable , thêm modal detail equipment, thêm function js  //get data and push to modal list trong các view:
     Chi_tiet_bao_duong, Chi_tiet_quyet_dinh, Chi_tiet_SCTX, chi _tiet_sua_chua, Chi_tiet_thanh_li, chi_tiet_thu_hoi
    - Thêm tiếng việt vào các Datatable trong tất cả các View

--- QUANGHANH2/Controllers/CDVT/Quyetdinh/ChitietThuhoiController.cs
+++ QUANGHANH2/Controllers/CDVT/Quyetdinh/ChitietThuhoiController.cs
@@ -14,7 +14,7 @@ namespace QUANGHANHCORE.Controllers.CDVT.Quyetdinh
         [Route("phong-cdvt/quyet-dinh/thu-hoi-chi-tiet")]
         public ActionResult LoadPage(String id)
         {
-            ViewBag.id = Base64Encode(id).ToString();
+            ViewBag.id = id.ToString();
             return View("/Views/CDVT/Quyet_dinh/Chi_tiet_thu_hoi.cshtml");
         }
 
@@ -33,7 +33,7 @@ namespace QUANGHANHCORE.Controllers.CDVT.Quyetdinh
         public ActionResult Detail()
         {
             string requestID = Request["sessionId"];
-            requestID = Base64Decode(requestID).ToString();
+            int id = Int32.Parse(requestID);
             int start = Convert.ToInt32(Request["start"]);
             int length = 10;
             string searchValue = Request["search[value]"];
@@ -49,10 +49,13 @@ namespace QUANGHANHCORE.Controllers.CDVT.Quyetdinh
                                                                          join b in db.Documentaries on a.documentary_id equals b.documentary_id
                                                                          join c in db.Equipments on a.equipmentId equals c.equipmentId
                                                                          join d in db.Departments on c.department_id equals d.department_id
-                                                                        where (b.documentary_code == requestID)
+                                                                        where (b.documentary_id == id)
                                                                         select new
                                                                          {
+                                                                            documentary_id = b.documentary_id,
                                                                             documentary_code = b.documentary_code,
+                                                                            equipmentId = c.equipmentId,
+                                                                            equipment_revoke_status = a.equipment_revoke_status,
                                                                             equipment_name = c.equipment_name,
                                                                              department_name = d.department_name,
                                                                              department_id = c.department_id,
@@ -60,8 +63,11 @@ namespace QUANGHANHCORE.Controllers.CDVT.Quyetdinh
                                                                        
                                                                          }).ToList().Select(p => new Documentary_revoke_detailsDB
                                                                          {
+                                                                             documentary_id = p.documentary_id,
                                                                              documentary_code = p.documentary_code,
+                                                                             equipmentId = p.equipmentId,
                                                                              equipment_name = p.equipment_name,
+                                                                             equipment_revoke_status = p.equipment_revoke_status,
                                                                              department_name = p.department_name,
                                                                              department_id = p.department_id,
                                                                              reason = p.reason,
@@ -69,6 +75,7 @@ namespace QUANGHANHCORE.Controllers.CDVT.Quyetdinh
                 foreach (var el in documentariesList)
                 {
                     el.order_number = count++;
+                    el.idAndEquip = el.documentary_id + "^" + el.equipmentId;
 
                 }
                 int totalrows = documentariesList.Count;
