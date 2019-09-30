83bdb7719c60f6f12b2b1e687a58f847b2ef7f41
duyntse04704@fpt.edu.vn <duyntse04704@fpt.edu.vn>
2019-09-30 16:03:34 +0700

    Add and Test Message Object

---
+++ QUANGHANH2/Models/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QUANGHANH2.Models
+{
+    public class Message
+    {
+        public string searchSuccessfully()
+        {
+            return "Tìm thành công";
+        }
+    }
+}
