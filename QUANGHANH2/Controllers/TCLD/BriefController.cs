c3f47a1c3dcb73573c84806f38de18b82dd934cb
Long Vu <vulong22121999@gmail.com>
2019-10-04 15:38:41 +0700

    update phân quyền hồ sơ trong công ty

--- QUANGHANH2/Controllers/TCLD/BriefController.cs
+++ QUANGHANH2/Controllers/TCLD/BriefController.cs
@@ -25,9 +25,9 @@ namespace QUANGHANHCORE.Controllers.TCLD
     public class BriefController : Controller
     {
         // GET: /<controller>/
-
+        
         public static string id_ = "";
-       
+        [Auther(RightID = "129")]
         [Route("phong-tcld/quan-ly-ho-so/ho-so-trong-cong-ty")]
         [HttpGet]
         public ActionResult Inside()
