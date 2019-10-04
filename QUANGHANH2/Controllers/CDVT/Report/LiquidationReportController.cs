6bb36e8d9351c93a317852e9bb45739c1ce96069
duongpl <51127228+duongpl@users.noreply.github.com>
2019-10-04 12:00:23 +0700

    update

--- QUANGHANH2/Controllers/CDVT/Report/LiquidationReportController.cs
+++ QUANGHANH2/Controllers/CDVT/Report/LiquidationReportController.cs
@@ -1,22 +1,148 @@
-﻿using System;
+﻿using OfficeOpenXml;
+using QUANGHANH2.Models;
+using QUANGHANH2.SupportClass;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Web.Hosting;
 using System.Web.Mvc;using System.Web.Routing;
 
 namespace QUANGHANHCORE.Controllers.CDVT.Report
 {
     public class LiquidationReportController : Controller
     {
+        [Auther(RightID ="46")]
         [Route("phong-cdvt/bao-cao/thanh-ly")]
-        public ActionResult Index()
+        public ActionResult Index(string type, string date, string month, string quarter, string year)
         {
+
             return View("/Views/CDVT/Report/LiquidationReport.cshtml");
         }
-        [Route("phong-cdvt/bao-cao/thanh-ly/excel")]
-        public ActionResult Export()
+        private void Wherecondition(string type, string date, string month, string quarter, string year)
         {
-            return File( "~/excel/CDVT/thanhly.xls", contentType: "text/plain; charset=utf-8", fileDownloadName: "thanhly.xls");
+            string query = "";
+            if (type == null)
+            {
+                var ngay = DateTime.Now.Date;
+                query = "select MONTH(a.date) as Thang, YEAR(a.date) as Nam,c.equipmentId as MaThietBi, " +
+                " equipment_name as TenThietBi,consumption_value as " +
+                " LuongTieuThu,ac.quantity as SanLuong from Fuel_activities_consumption a , Supply b, Equipment c , Activity ac " +
+                " where a.fuel_type = b.supply_id and a.equipmentId = c.equipmentId  and ac.equipmentId = c.equipmentId " +
+                " and fuel_type in ('DIEN') and a.date = '" + ngay + "' and a.date = ac.date";
+            }
+            if (type == "day")
+            {
+                var ngay = DateTime.ParseExact(date, "dd/MM/yyyy", null).ToString("yyyy-MM-dd");
+                query = "select MONTH(a.date) as Thang, YEAR(a.date) as Nam,c.equipmentId as MaThietBi, " +
+                " equipment_name as TenThietBi,consumption_value as " +
+                " LuongTieuThu,ac.quantity as SanLuong from Fuel_activities_consumption a , Supply b, Equipment c , Activity ac " +
+                " where a.fuel_type = b.supply_id and a.equipmentId = c.equipmentId  and ac.equipmentId = c.equipmentId " +
+                " and fuel_type in ('DIEN') and a.date = '" + ngay + "' and a.date = ac.date";
+                ViewBag.now = date;
+            }
+            if (type == "month")
+            {
+                int thang = Convert.ToInt32(month);
+                int nam = Convert.ToInt32(year);
+                query = "select MONTH(a.date) as Thang, YEAR(a.date) as Nam,c.equipmentId as MaThietBi, " +
+                " equipment_name as TenThietBi,consumption_value as " +
+                " LuongTieuThu,ac.quantity as SanLuong from Fuel_activities_consumption a , Supply b, Equipment c , Activity ac " +
+                " where a.fuel_type = b.supply_id and a.equipmentId = c.equipmentId  and ac.equipmentId = c.equipmentId " +
+                " and fuel_type in ('DIEN') and a.date = ac.date and YEAR(a.date) = " + nam + " and MONTH(a.date) = " + thang;
+            }
+            if (type == "quarter")
+            {
+                int nam = Convert.ToInt32(year);
+                string quy = "";
+                if (quarter == "1")
+                {
+                    quy = " (1,2,3) ";
+                }
+                if (quarter == "2")
+                {
+                    quy = " (4,5,6) ";
+                }
+                if (quarter == "3")
+                {
+                    quy = " (7,8,9) ";
+                }
+                if (quarter == "4")
+                {
+                    quy = " (10,11,12) ";
+                }
+                query = "select MONTH(a.date) as Thang, YEAR(a.date) as Nam,c.equipmentId as MaThietBi, " +
+                " equipment_name as TenThietBi,consumption_value as " +
+                " LuongTieuThu,ac.quantity as SanLuong from Fuel_activities_consumption a , Supply b, Equipment c , Activity ac " +
+                " where a.fuel_type = b.supply_id and a.equipmentId = c.equipmentId  and ac.equipmentId = c.equipmentId " +
+                " and fuel_type in ('DIEN') and a.date = ac.date and YEAR(a.date) = " + nam + " and Month(a.date) in " + quy;
+            }
+            if (type == "year")
+            {
+                int nam = Convert.ToInt32(year);
+                query = " select MONTH(a.date) as Thang, YEAR(a.date) as Nam,c.equipmentId as MaThietBi, " +
+                " equipment_name as TenThietBi,consumption_value as " +
+                " LuongTieuThu,ac.quantity as SanLuong from Fuel_activities_consumption a , Supply b, Equipment c , Activity ac " +
+                " where a.fuel_type = b.supply_id and a.equipmentId = c.equipmentId  and ac.equipmentId = c.equipmentId " +
+                " and fuel_type in ('DIEN') and a.date = ac.date and YEAR(a.date) = " + nam;
+            }
+
+            using (QUANGHANHABCEntities db = new QUANGHANHABCEntities())
+            {
+                ViewBag.ContentReport = db.Database.SqlQuery<contentreportPower>(query).ToList();
+            }
+        }
+        [Route("phong-cdvt/bao-cao/thanh-ly/excel")]
+        public ActionResult Export(string type, string date, string month, string quarter, string year)
+        {
+            string path = HostingEnvironment.MapPath("/excel/CDVT/diennang.xlsx");
+            string saveAsPath = ("/excel/CDVT/download/diennang.xlsx");
+            FileInfo file = new FileInfo(path);
+            using (ExcelPackage excelPackage = new ExcelPackage(file))
+            {
+                ExcelWorkbook excelWorkbook = excelPackage.Workbook;
+                ExcelWorksheet excelWorksheet = excelWorkbook.Worksheets.First();
+                using (QUANGHANHABCEntities db = new QUANGHANHABCEntities())
+                {
+                    Wherecondition(type, date, month, quarter, year);
+                    int totaltieuthu = 0; int totalsanluong = 0;
+                    if (ViewBag.ContentReport != null)
+                    {
+                        foreach (var item in ViewBag.ContentReport)
+                        {
+                            totaltieuthu += item.LuongTieuThu;
+                        }
+                    }
+                    if (ViewBag.ContentReport != null)
+                    {
+                        foreach (var item in ViewBag.ContentReport)
+                        {
+                            totalsanluong += item.SanLuong;
+                        }
+                    }
+                    int k = 3;
+                    List<contentreportPower> content = ViewBag.ContentReport;
+                    for (int i = 0; i < content.Count; i++)
+                    {
+                        excelWorksheet.Cells[k, 1].Value = content.ElementAt(i).Thang + "/" + content.ElementAt(i).Nam;
+                        excelWorksheet.Cells[k, 2].Value = content.ElementAt(i).MaThietBi;
+                        excelWorksheet.Cells[k, 3].Value = content.ElementAt(i).TenThietBi;
+                        excelWorksheet.Cells[k, 4].Value = content.ElementAt(i).LuongTieuThu;
+                        excelWorksheet.Cells[k, 5].Value = content.ElementAt(i).SanLuong;
+                        k++;
+                    }
+                    excelWorksheet.Cells[k, 3].Value = "Tổng";
+                    excelWorksheet.Cells[k, 4].Value = totaltieuthu;
+                    excelWorksheet.Cells[k, 5].Value = totalsanluong;
+                    excelWorksheet.Cells[k, 3].Style.Font.Bold = true;
+                    excelWorksheet.Cells[k, 3].Style.Font.Color.SetColor(System.Drawing.Color.Red);
+                    excelWorksheet.Cells[k, 4].Style.Font.Color.SetColor(System.Drawing.Color.Red);
+                    excelWorksheet.Cells[k, 5].Style.Font.Color.SetColor(System.Drawing.Color.Red);
+                    excelPackage.SaveAs(new FileInfo(HostingEnvironment.MapPath(saveAsPath)));
+                }
+            }
+            return Json(new { success = true, location = saveAsPath }, JsonRequestBehavior.AllowGet);
         }
     }
 }
