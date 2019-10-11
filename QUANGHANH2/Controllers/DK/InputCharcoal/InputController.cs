cf24674563b2ec79cab59a5508fbd339f195d348
sonnh <yggdrasil2769@gmail.com>
2019-10-11 17:09:49 +0700

    backend nhap lieu

--- QUANGHANH2/Controllers/DK/InputCharcoal/InputController.cs
+++ QUANGHANH2/Controllers/DK/InputCharcoal/InputController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QUANGHANH2.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -22,15 +23,28 @@ namespace QUANGHANH2.Controllers.DK.InputCharcoal
         [HttpPost]
         public JsonResult Change(string px_value, string to_value, string date)
         {
-            ViewBag.px = px_value;
-
-            return Json(new { success = true }, JsonRequestBehavior.AllowGet);
+            List<TieuChi> tcList = null;
+            QUANGHANHABCEntities db = new QUANGHANHABCEntities();
+            if (px_value != null)
+            {
+                if (px_value.Contains("KT"))
+                {
+                    string query = "select * from TieuChi where MaTieuChi in (2,6,9)";
+                    tcList = db.Database.SqlQuery<TieuChi>(query).ToList();
+                }
+                else if (px_value.Contains("DL"))
+                {
+                    tcList = db.TieuChis.SqlQuery("select * from TieuChi where MaTieuChi in (1,6,9)").ToList();
+                }
+            }
+            return Json(new { success = true, list = tcList }, JsonRequestBehavior.AllowGet);
         }
 
-        public JsonResult SaveChange(string date, string px_value, string to_value, 
-            string[] thucHien, string []keHoach, string []chenhLech, string []phanTramTH, 
-            string []luyKe, string []KHDC, string [] phanTramTD, string []tong, string[]motNgay, string[]ghiChu)
+        public JsonResult SaveChange(string date, string px_value, string ca_value,
+            string[] thucHien, string[] keHoach, string[] chenhLech, string[] phanTramTH,
+            string[] luyKe, string[] KHDC, string[] phanTramTD, string[] tong, string[] motNgay, string[] ghiChu)
         {
+
             return Json("", JsonRequestBehavior.AllowGet);
         }
     }
