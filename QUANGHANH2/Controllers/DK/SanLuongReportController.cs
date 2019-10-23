4d9d518e254b89b48d1660d3fab6cfa7c4565c29
duynguyenfpt <duyntse04704@fpt.edu.vn>
2019-10-23 10:59:08 +0700

    Thêm màn hình View trung gian

--- QUANGHANH2/Controllers/DK/SanLuongReportController.cs
+++ QUANGHANH2/Controllers/DK/SanLuongReportController.cs
@@ -11,6 +11,13 @@ namespace QUANGHANH2.Controllers.DK
 {
     public class SanLuongReportController : Controller
     {
+        [HttpGet]
+        [Route("phong-dieu-khien/bao-cao-san-xuat-than/san-luong-toan-cong-ty")]
+        public ActionResult getView()
+        {
+            return View("/Views/DK/SanLuongView.cshtml");
+        }
+        //
         // GET: SanLuongReport
         [HttpGet]
         [Route("phong-dieu-khien/bao-cao-san-xuat-than/bao-cao-san-luong-toan-cong-ty")]
@@ -38,43 +45,38 @@ namespace QUANGHANH2.Controllers.DK
             return item1;
         }
 
-        [HttpPost]
-        [Route("phong-dieu-khien/bao-cao-san-xuat-than/bao-cao-san-luong-toan-cong-ty")]
-        public ActionResult getReport()
+        dynamic getData(DateTime timeStart, DateTime timeEnd)
         {
-            DateTime timeEnd = Convert.ToDateTime("2019-09-10");
-            var timeStart = Convert.ToDateTime("" + timeEnd.Year + "-" + timeEnd.Month + "-1");
-            //
             var query = "select TieuChi.MaTieuChi,TieuChi.TenTieuChi, TieuChi.MaNhomTieuChi,NhomTieuChi.TenNhomTieuChi,MaPhongBan," +
-                "(Case when CA1 IS NULL then CONVERT(float, 0) else CA1 end) as [CA1]," +
-                "(Case when CA2 IS NULL then CONVERT(float,0) else CA2 end) as [CA2], " +
-                "(Case when CA3 IS NULL then CONVERT(float,0) else CA3 end) as [CA3], " +
-                "(Case when TH IS NULL then CONVERT(float,0) else TH end) as TH, " +
-                "(Case when LUYKE IS NULL then CONVERT(float,0) else LUYKE end) as LUYKE, " +
-                "(Case when KH IS NULL then CONVERT(float,0) else KH end) as KH, " +
-                "(Case when CHENHLECH IS NULL then CONVERT(float,0) else CHENHLECH end) as [CHENHLECH], " +
-                "(Case when[PERCENTAGE] IS NULL then CONVERT(float,0) else [PERCENTAGE] end) as [PERCENTAGE], " +
-                "(Case when KHDC IS NULL then CONVERT(float,0) else KHDC end) as KHDC, " +
-                "(Case when percentDC IS NULL then 0 else percentDC end) as percentDC, " +
-                "(Case when LUYKE IS NULL then 0 else LUYKE end) as LUYKE, " +
-                "(Case when KH IS NULL then 0 else KH end) as KH from TieuChi " +
-                "left join (select * ,CONVERT(float, 0) as [KH]," +
-                "CONVERT(float, 0) as [CHENHLECH],CONVERT(float, 0) as [PERCENTAGE], " +
-                "CONVERT(float, 0) as [KHDC], CONVERT(float, 0) as [percentDC], " +
-                "CONVERT(float, 0) as [SUM], CONVERT(float, 0) as [perday],  " +
-                "CONVERT(float, 0) as [BQKHDC] from(select MaTieuChi, MaPhongBan, " +
-                "Sum(case when ca = 1 and Ngay = @dateEnd then SanLuong else 0  end )as [CA1], " +
-                "Sum(case when ca = 2 and Ngay = @dateEnd then SanLuong else 0  end )as [CA2], " +
-                "Sum(case when ca = 3 and Ngay = @dateEnd then SanLuong else 0  end )as [CA3], " +
-                "Sum(case when Ngay = @dateEnd then SanLuong else 0  end )as [TH],  " +
-                "SUM(SanLuong) as [LUYKE] from(" +
-                "select header_th.MaPhongBan, thuchien.HeaderID, thuchien.MaTieuChi, thuchien.SanLuong, header_th.Ca, header_th.Ngay, px.department_id, px.isInside " +
-                "from ThucHien_TieuChi_TheoNgay as thuchien inner JOIN header_ThucHienTheoNgay as header_th " +
-                "on thuchien.HeaderID = header_th.HeaderID and header_th.Ngay >= @dateStart and header_th.Ngay <= @dateEnd " +
-                "INNER JOIN Department as px on px.department_id = header_th.MaPhongBan) as a GROUP BY MaTieuChi,MaPhongBan) as table2 ) " +
-                "as table3 on table3.MaTieuChi = TieuChi.MaTieuChi " +
-                "inner join NhomTieuChi on TieuChi.MaNhomTieuChi = NhomTieuChi.MaNhomTieuChi " +
-                "order by MaTieuChi";
+               "(Case when CA1 IS NULL then CONVERT(float, 0) else CA1 end) as [CA1]," +
+               "(Case when CA2 IS NULL then CONVERT(float,0) else CA2 end) as [CA2], " +
+               "(Case when CA3 IS NULL then CONVERT(float,0) else CA3 end) as [CA3], " +
+               "(Case when TH IS NULL then CONVERT(float,0) else TH end) as TH, " +
+               "(Case when LUYKE IS NULL then CONVERT(float,0) else LUYKE end) as LUYKE, " +
+               "(Case when KH IS NULL then CONVERT(float,0) else KH end) as KH, " +
+               "(Case when CHENHLECH IS NULL then CONVERT(float,0) else CHENHLECH end) as [CHENHLECH], " +
+               "(Case when[PERCENTAGE] IS NULL then CONVERT(float,0) else [PERCENTAGE] end) as [PERCENTAGE], " +
+               "(Case when KHDC IS NULL then CONVERT(float,0) else KHDC end) as KHDC, " +
+               "(Case when percentDC IS NULL then 0 else percentDC end) as percentDC, " +
+               "(Case when LUYKE IS NULL then 0 else LUYKE end) as LUYKE, " +
+               "(Case when KH IS NULL then 0 else KH end) as KH from TieuChi " +
+               "left join (select * ,CONVERT(float, 0) as [KH]," +
+               "CONVERT(float, 0) as [CHENHLECH],CONVERT(float, 0) as [PERCENTAGE], " +
+               "CONVERT(float, 0) as [KHDC], CONVERT(float, 0) as [percentDC], " +
+               "CONVERT(float, 0) as [SUM], CONVERT(float, 0) as [perday],  " +
+               "CONVERT(float, 0) as [BQKHDC] from(select MaTieuChi, MaPhongBan, " +
+               "Sum(case when ca = 1 and Ngay = @dateEnd then SanLuong else 0  end )as [CA1], " +
+               "Sum(case when ca = 2 and Ngay = @dateEnd then SanLuong else 0  end )as [CA2], " +
+               "Sum(case when ca = 3 and Ngay = @dateEnd then SanLuong else 0  end )as [CA3], " +
+               "Sum(case when Ngay = @dateEnd then SanLuong else 0  end )as [TH],  " +
+               "SUM(SanLuong) as [LUYKE] from(" +
+               "select header_th.MaPhongBan, thuchien.HeaderID, thuchien.MaTieuChi, thuchien.SanLuong, header_th.Ca, header_th.Ngay, px.department_id, px.isInside " +
+               "from ThucHien_TieuChi_TheoNgay as thuchien inner JOIN header_ThucHienTheoNgay as header_th " +
+               "on thuchien.HeaderID = header_th.HeaderID and header_th.Ngay >= @dateStart and header_th.Ngay <= @dateEnd " +
+               "INNER JOIN Department as px on px.department_id = header_th.MaPhongBan) as a GROUP BY MaTieuChi,MaPhongBan) as table2 ) " +
+               "as table3 on table3.MaTieuChi = TieuChi.MaTieuChi " +
+               "inner join NhomTieuChi on TieuChi.MaNhomTieuChi = NhomTieuChi.MaNhomTieuChi " +
+               "order by MaTieuChi";
 
             var query_KHDC = "select (case when table1.SanLuong is null then 0 else table1.SanLuong end) as SanLuong," +
                 "(case when table1.MaPhongBan is null then '' else table1.MaPhongBan end) as MaPhongBan,TieuChi.MaTieuChi from (select MaTieuChi, SUM(SanLuong) as SanLuong,header.MaPhongBan from(" +
@@ -236,43 +238,55 @@ namespace QUANGHANH2.Controllers.DK
                 // Tong met lo CBSX = Met Lo Tu Lam + Met Lo Thue Ngoai
                 reports[11] = addUp(reports[11], reports[12]);
                 reports[11] = addUp(reports[11], reports[15]);
-                JsonSerializerSettings jss = new JsonSerializerSettings { ReferenceLoopHandling = ReferenceLoopHandling.Ignore };
-                var result = JsonConvert.SerializeObject(reports, Formatting.Indented, jss);
-                return Json(new { success = true, data = result }, JsonRequestBehavior.AllowGet);
+                return reports;
             }
         }
-    }
 
-    public class KHDCEntity
-    {
-        public int MaTieuChi { get; set; }
-        public double SanLuong { get; set; }
-        public string MaPhongBan { get; set; }
-    }
 
-    public class reportEntity
-    {
-        public int MaTieuChi { get; set; }
-        public string TenTieuChi { get; set; }
-        public double Ca1 { get; set; }
-        public double Ca2 { get; set; }
-        public double Ca3 { get; set; }
-        public double TH { get; set; }
-        public double KH { get; set; }
-        public double luyke { get; set; }
-        public double chenhlech { get; set; }
-        public double percentage { get; set; }
-        public double KHDC { get; set; }
-        public double percentageDC { get; set; }
-        public double SUM { get; set; }
-        public double perday { get; set; }
-
-        public double BQQHDC { get; set; }
-        public string GhiChu { get; set; }
-        public string TenPhongBan { get; set; }
-        public bool isHeader { get; set; }
-        public int MaNhomTieuChi { get; set; }
-        public string TenNhomTieuChi { get; set; }
-        public string MaPhongBan { get; set; }
+        [HttpPost]
+        [Route("phong-dieu-khien/bao-cao-san-xuat-than/bao-cao-san-luong-toan-cong-ty")]
+        public ActionResult getReport()
+        {
+            DateTime timeEnd = Convert.ToDateTime("2019-09-10");
+            var timeStart = Convert.ToDateTime("" + timeEnd.Year + "-" + timeEnd.Month + "-1");
+            //
+            var reports = getData(timeStart, timeEnd);
+            JsonSerializerSettings jss = new JsonSerializerSettings { ReferenceLoopHandling = ReferenceLoopHandling.Ignore };
+            var result = JsonConvert.SerializeObject(reports, Formatting.Indented, jss);
+            return Json(new { success = true, data = result }, JsonRequestBehavior.AllowGet);
+        }
     }
+}
+
+public class KHDCEntity
+{
+    public int MaTieuChi { get; set; }
+    public double SanLuong { get; set; }
+    public string MaPhongBan { get; set; }
+}
+
+public class reportEntity
+{
+    public int MaTieuChi { get; set; }
+    public string TenTieuChi { get; set; }
+    public double Ca1 { get; set; }
+    public double Ca2 { get; set; }
+    public double Ca3 { get; set; }
+    public double TH { get; set; }
+    public double KH { get; set; }
+    public double luyke { get; set; }
+    public double chenhlech { get; set; }
+    public double percentage { get; set; }
+    public double KHDC { get; set; }
+    public double percentageDC { get; set; }
+    public double SUM { get; set; }
+    public double perday { get; set; }
+
+    public double BQQHDC { get; set; }
+    public string GhiChu { get; set; }
+    public string TenPhongBan { get; set; }
+    public bool isHeader { get; set; }
+    public int MaNhomTieuChi { get; set; }
+    public string TenNhomTieuChi { get; set; }
+    public string MaPhongBan { get; set; }
 }
