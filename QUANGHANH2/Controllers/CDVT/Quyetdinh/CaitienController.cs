68636cd6a5b092feaf13644d30b4b6dda4a3a0eb
thang1160 <thang1160@users.noreply.github.com>
2019-10-28 01:01:21 +0700

    Thêm tạo quyết định cải tiến

--- QUANGHANH2/Controllers/CDVT/Quyetdinh/CaitienController.cs
+++ QUANGHANH2/Controllers/CDVT/Quyetdinh/CaitienController.cs
@@ -1,10 +1,16 @@
-﻿using QUANGHANH2.Models;
+﻿using OfficeOpenXml;
+using QUANGHANH2.Models;
 using QUANGHANH2.SupportClass;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.SqlClient;
+using System.Globalization;
+using System.IO;
 using System.Linq;
+using System.Linq.Dynamic;
 using System.Web;
+using System.Web.Hosting;
 using System.Web.Mvc;
 
 namespace QUANGHANH2.Controllers.CDVT.Quyetdinh
@@ -119,8 +125,8 @@ namespace QUANGHANH2.Controllers.CDVT.Quyetdinh
                     {
                         try
                         {
-                            List<Documentary_maintain_details> details = DBContext.Documentary_maintain_details.Where(x => x.documentary_id == documentary_id).ToList();
-                            foreach (Documentary_maintain_details item in details)
+                            List<Documentary_Improve_Detail> details = DBContext.Documentary_Improve_Detail.Where(x => x.documentary_id == documentary_id).ToList();
+                            foreach (Documentary_Improve_Detail item in details)
                             {
                                 Equipment e = DBContext.Equipments.Find(item.equipmentId);
                                 e.current_Status = 5;
@@ -148,6 +154,164 @@ namespace QUANGHANH2.Controllers.CDVT.Quyetdinh
                             }, JsonRequestBehavior.AllowGet);
                         }
                     }
+                }
+            }
+        }
+
+        [HttpPost]
+        public ActionResult GetById(List<String> docID)
+        {
+            string id = docID[0];
+            try
+            {
+                QUANGHANHABCEntities DBContext = new QUANGHANHABCEntities();
+                Documentary_Extend documentaryList = DBContext.Database.SqlQuery<Documentary_Extend>("Select documentary_id,documentary_code,department_id,person_created,date_created,reason, [out/in_come] as out_in_come from Documentary where documentary_id = @documentary_id", 
+                    new SqlParameter("documentary_id", id)).First();
+                documentaryList.tempId = id;
+                ViewBag.ID = id;
+                documentaryList.date_created = DateTime.Now;
+                return Json(documentaryList);
+            }
+            catch (Exception)
+            {
+                return Json(new { success = false, message = "Có lỗi xảy ra" });
+            }
+        }
+
+        [HttpPost]
+        public ActionResult DeleteDoc(int docID)
+        {
+            using (QUANGHANHABCEntities db = new QUANGHANHABCEntities())
+            {
+                Documentary doc = db.Documentaries.Where(x => x.documentary_id == docID).FirstOrDefault<Documentary>();
+                db.Documentaries.Remove(doc);
+                db.SaveChanges();
+                return Json(new { success = true, message = "Xóa thành công!" });
+            }
+        }
+
+        [Route("phong-cdvt/quyet-dinh/cai-tien/search")]
+        [HttpPost]
+        public ActionResult Search(string person_created, string dateStart, string dateEnd)
+        {
+            int start = Convert.ToInt32(Request["start"]);
+            int length = Convert.ToInt32(Request["length"]);
+            string searchValue = Request["search[value]"];
+            string sortColumnName = Request["columns[" + Request["order[0][column]"] + "][name]"];
+            string sortDirection = Request["order[0][dir]"];
+            List<Documentary_Extend> documentaryList = new List<Documentary_Extend>();
+            DateTime dtEnd;
+            DateTime dtStart;
+            try
+            {
+                if (dateStart == "Nhập ngày bắt đầu (từ)") dateStart = "01/01/1900";
+                dtStart = DateTime.ParseExact(dateStart, "dd/MM/yyyy", CultureInfo.InvariantCulture);
+                if (dateEnd == "Nhập ngày kết thúc (đến)") dtEnd = DateTime.Now;
+                else dtEnd = DateTime.ParseExact(dateEnd, "dd/MM/yyyy", CultureInfo.InvariantCulture);
+                dtEnd = dtEnd.AddHours(23);
+                dtEnd = dtEnd.AddMinutes(59);
+            }
+            catch
+            {
+                return Json(new { success = false, message = "Vui lòng nhập đúng ngày tháng năm" });
+            }
+            QUANGHANHABCEntities db = new QUANGHANHABCEntities();
+
+
+            documentaryList = (from document in db.Documentaries
+                               where document.documentary_type.Equals("7") && document.person_created.Contains(person_created) && (document.date_created >= dtStart && document.date_created <= dtEnd)
+                               join detail in db.Documentary_Improve_Detail on document.documentary_id equals detail.documentary_id
+                               into temporary
+                               select new
+                               {
+                                   documentary_id = document.documentary_id,
+                                   documentary_code = document.documentary_code,
+                                   date_created = document.date_created,
+                                   person_created = document.person_created,
+                                   reason = document.reason,
+                                   out_in_come = document.out_in_come,
+                                   count = temporary.Select(x => new { x.equipmentId }).Count()
+                               }).ToList().Select(p => new Documentary_Extend
+                               {
+                                   documentary_id = p.documentary_id,
+                                   documentary_code = p.documentary_code,
+                                   date_created = p.date_created,
+                                   person_created = p.person_created,
+                                   reason = p.reason,
+                                   out_in_come = p.out_in_come,
+                                   count = p.count
+                               }).ToList();
+            foreach (var el in documentaryList)
+            {
+                if (el.documentary_code == null || el.documentary_code.Equals(""))
+                {
+                    el.tempId = el.documentary_id + "^false";
+                }
+                else
+                {
+                    el.tempId = el.documentary_id + "^true^" + el.documentary_code;
+                }
+
+            }
+            int totalrows = documentaryList.Count;
+            int totalrowsafterfiltering = documentaryList.Count;
+            //sorting
+            documentaryList = documentaryList.OrderBy(sortColumnName + " " + sortDirection).ToList<Documentary_Extend>();
+            //paging
+            documentaryList = documentaryList.Skip(start).Take(length).ToList<Documentary_Extend>();
+
+            return Json(new { success = true, data = documentaryList, draw = Request["draw"], recordsTotal = totalrows, recordsFiltered = totalrowsafterfiltering }, JsonRequestBehavior.AllowGet);
+        }
+
+        public void ExportExcel()
+        {
+            string path = HostingEnvironment.MapPath("/excel/CDVT/danhsachsuachua_Template.xlsx");
+            FileInfo file = new FileInfo(path);
+
+            using (ExcelPackage excelPackage = new ExcelPackage(file))
+            {
+                ExcelWorkbook excelWorkbook = excelPackage.Workbook;
+                ExcelWorksheet excelWorksheet = excelWorkbook.Worksheets.First();
+
+                using (QUANGHANHABCEntities db = new QUANGHANHABCEntities())
+                {
+                    List<Documentary_Extend> documentaryList = (from document in db.Documentaries
+                                                                where (document.documentary_type.Equals("2") && (document.documentary_code == "" || document.documentary_code == null))
+                                                                join detail in db.Documentary_Improve_Detail on document.documentary_id equals detail.documentary_id
+                                                                into temporary
+                                                                select new
+                                                                {
+
+                                                                    date_created = document.date_created,
+                                                                    documentary_code = document.documentary_code,
+                                                                    person_created = document.person_created,
+                                                                    reason = document.reason,
+                                                                    out_in_come = document.out_in_come,
+                                                                    count = temporary.Select(x => new { x.equipmentId }).Count()
+                                                                }).ToList().Select(p => new Documentary_Extend
+                                                                {
+
+                                                                    date_created = p.date_created,
+                                                                    documentary_code = p.documentary_code,
+                                                                    person_created = p.person_created,
+                                                                    reason = p.reason,
+                                                                    out_in_come = p.out_in_come,
+                                                                    count = p.count
+                                                                }).ToList();
+                    int k = 0;
+                    for (int i = 2; i < documentaryList.Count + 2; i++)
+                    {
+                        excelWorksheet.Cells[i, 1].Value = (k + 1);
+                        excelWorksheet.Cells[i, 2].Value = documentaryList.ElementAt(k).date_created.ToString("hh:mm tt dd/MM/yyyy");
+                        excelWorksheet.Cells[i, 3].Value = documentaryList.ElementAt(k).documentary_code;
+                        excelWorksheet.Cells[i, 4].Value = documentaryList.ElementAt(k).person_created;
+                        excelWorksheet.Cells[i, 5].Value = documentaryList.ElementAt(k).count;
+                        excelWorksheet.Cells[i, 6].Value = documentaryList.ElementAt(k).reason;
+                        excelWorksheet.Cells[i, 7].Value = documentaryList.ElementAt(k).out_in_come;
+                        k++;
+                    }
+                    string location = HostingEnvironment.MapPath("/excel/CDVT/download");
+                    excelPackage.SaveAs(new FileInfo(location + "/BaoDuongThietBi.xlsx"));
                 }
             }
         }
