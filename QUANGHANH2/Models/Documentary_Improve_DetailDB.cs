647865c2666bb7668dc6c46b39915fe20722ae50
thang1160 <thang1160@users.noreply.github.com>
2019-11-01 12:43:40 +0700

    fix bug xử lý và nghiệm thu 7 loại quyết định

---
+++ QUANGHANH2/Models/Documentary_Improve_DetailDB.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QUANGHANH2.Models
+{
+    public class Documentary_Improve_DetailDB : Documentary_Improve_Detail
+    {
+        public string equipment_name { get; set; }
+        public string statusAndEquip { get; set; }
+
+        public string department_name { get; set; }
+        public string department_id { get; set; }
+        public string reason { get; set; }
+        public int order_number { get; set; }
+        public string documentary_code { get; set; }
+        public string idAndEquip { get; set; }
+    }
+}
