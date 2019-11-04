b6bb69c8258d942068c2beeb602b8cc90546613d
hvu0163 <53392914+hvu0163@users.noreply.github.com>
2019-11-04 17:28:44 +0700

    CRUD camera

--- QUANGHANH2/Controllers/Camera/CameraManagementController.cs
+++ QUANGHANH2/Controllers/Camera/CameraManagementController.cs
@@ -94,7 +94,38 @@ namespace QUANGHANH2.Controllers.Camera
                     return Json(new { success = false, message = e.Message }, JsonRequestBehavior.AllowGet);
                 }
             }
-            return View(new Equipment());
+        }
+
+        [HttpPost]
+        public ActionResult Edit(string code, string name, string room, string note, string seri, int capacty, string disk_status, string cam_status)
+        {
+            string sql = "update Camera set camera_name = @name, camera_status = @cam_status, room_id = @room, note = @note where camera_id = @code";
+            string query = "update Disk set disk_status = @disk, capacity = @cap where series = @seri and camera_id = @code";
+            QUANGHANHABCEntities db = new QUANGHANHABCEntities();
+            using (DbContextTransaction dbc = db.Database.BeginTransaction())
+            {
+                try
+                {
+                    db.Database.ExecuteSqlCommand(sql, new SqlParameter("code", code)
+                                                        , new SqlParameter("name", name)
+                                                        , new SqlParameter("cam_status", cam_status)
+                                                        , new SqlParameter("note", note)
+                                                        , new SqlParameter("room", room));
+                    db.Database.ExecuteSqlCommand(query, new SqlParameter("code", code)
+                                                        , new SqlParameter("seri", seri)
+                                                        , new SqlParameter("cap", capacty)
+                                                        , new SqlParameter("disk", disk_status));
+                    dbc.Commit();
+                    db.SaveChanges();
+                    return RedirectToAction("GetData");
+                }
+                catch (Exception e)
+                {
+                    e.Message.ToString();
+                    dbc.Rollback();
+                    return Json(new { success = false, message = e.Message }, JsonRequestBehavior.AllowGet);
+                }
+            }
         }
 
         public class camDB : Models.Camera
