f9b5e1ae1bbbcdfc49777176267d7550cdfeae25
N0sBa <nguyenbason.fpt04041999@gmail.com>
2019-11-07 18:09:08 +0700

    Cập nhật màn hình liên quan đến Công Việc

---
+++ QUANGHANH2/Controllers/TCLD/Occupation/SideOccupationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QUANGHANH2.Controllers.TCLD.Occupation
+{
+    public class SideOccupationController : Controller
+    {
+        [Route("phong-tcld/quan-ly-dien-cong-viec")]
+        public ActionResult Index()
+        {
+            return View("/Views/TCLD/Occupation/SideOccupation.cshtml");
+        }
+    }
+}
