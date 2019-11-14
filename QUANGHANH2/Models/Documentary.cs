f5e576e22b3136036ad39bada4c9aff80efeb3dd
N0sBa <nguyenbason.fpt04041999@gmail.com>
2019-11-14 19:42:12 +0700

    Cập nhật DB + gen lại Model

--- QUANGHANH2/Models/Documentary.cs
+++ QUANGHANH2/Models/Documentary.cs
@@ -19,14 +19,14 @@ namespace QUANGHANH2.Models
         {
             this.Acceptances = new HashSet<Acceptance>();
             this.Camera_Acceptance = new HashSet<Camera_Acceptance>();
-            this.Documentary_camera_repair_details = new HashSet<Documentary_camera_repair_details>();
-            this.Documentary_Improve_Detail = new HashSet<Documentary_Improve_Detail>();
             this.Documentary_big_maintain_details = new HashSet<Documentary_big_maintain_details>();
             this.Documentary_liquidation_details = new HashSet<Documentary_liquidation_details>();
             this.Documentary_maintain_details = new HashSet<Documentary_maintain_details>();
             this.Documentary_moveline_details = new HashSet<Documentary_moveline_details>();
             this.Documentary_repair_details = new HashSet<Documentary_repair_details>();
             this.Documentary_revoke_details = new HashSet<Documentary_revoke_details>();
+            this.Documentary_camera_repair_details = new HashSet<Documentary_camera_repair_details>();
+            this.Documentary_Improve_Detail = new HashSet<Documentary_Improve_Detail>();
             this.Supply_Documentary_Camera = new HashSet<Supply_Documentary_Camera>();
             this.Supply_Documentary_Equipment = new HashSet<Supply_Documentary_Equipment>();
         }
@@ -46,11 +46,6 @@ namespace QUANGHANH2.Models
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Camera_Acceptance> Camera_Acceptance { get; set; }
         public virtual Department Department { get; set; }
-        public virtual DocumentaryType DocumentaryType { get; set; }
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        public virtual ICollection<Documentary_camera_repair_details> Documentary_camera_repair_details { get; set; }
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        public virtual ICollection<Documentary_Improve_Detail> Documentary_Improve_Detail { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Documentary_big_maintain_details> Documentary_big_maintain_details { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
@@ -63,6 +58,11 @@ namespace QUANGHANH2.Models
         public virtual ICollection<Documentary_repair_details> Documentary_repair_details { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Documentary_revoke_details> Documentary_revoke_details { get; set; }
+        public virtual DocumentaryType DocumentaryType { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Documentary_camera_repair_details> Documentary_camera_repair_details { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Documentary_Improve_Detail> Documentary_Improve_Detail { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Supply_Documentary_Camera> Supply_Documentary_Camera { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
