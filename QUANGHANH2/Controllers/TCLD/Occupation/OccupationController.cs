c8347c19ec7de7cd3cc432af0179969396b6c59e
N0sBa <nguyenbason.fpt04041999@gmail.com>
2019-11-22 21:00:06 +0700

    Cập nhật màn hình Công Việc

--- QUANGHANH2/Controllers/TCLD/Occupation/OccupationController.cs
+++ QUANGHANH2/Controllers/TCLD/Occupation/OccupationController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QUANGHANH2.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +13,40 @@ namespace QUANGHANH2.Controllers.TCLD
         public ActionResult Index()
         {
             return View("/Views/TCLD/Occupation/Occupation.cshtml");
+        }
+
+        ////////////////////////////LIST/////////////////////////////
+        [Route("phong-tcld/quan-ly-cong-viec/danh-sach-cong-viec")]
+        [HttpGet]
+        public ActionResult list()
+        {
+            using (QUANGHANHABCEntities db = new QUANGHANHABCEntities())
+            {
+                db.Configuration.LazyLoadingEnabled = true;
+
+                //get data paging
+                int start = Convert.ToInt32(Request["start"]);
+                int length = Convert.ToInt32(Request["length"]);
+                string searchValue = Request["search[value]"];
+                string sortColumnName = Request["columns[" + Request["order[0][column]"] + "][name]"];
+                string sortDirection = Request["order[0][dir]"];
+
+                try
+                {
+                    List<CongViec> congviec_list = new List<CongViec>();
+                    var sqlList = "select * from CongViec order by " + sortColumnName + " " + sortDirection + " OFFSET " + start + " ROWS FETCH NEXT " + length + " ROWS ONLY";
+                    congviec_list = db.Database.SqlQuery<CongViec>(sqlList).ToList();
+                    int totalrows = db.CongViecs.Count();
+
+                    int totalrowsafterfiltering = totalrows;
+                    return Json(new { congviec_list = congviec_list, recordsTotal = totalrows, recordsFiltered = totalrowsafterfiltering }, JsonRequestBehavior.AllowGet);
+                }
+                catch (Exception e)
+                {
+
+                }
+                return null;
+            }
         }
     }
 }
