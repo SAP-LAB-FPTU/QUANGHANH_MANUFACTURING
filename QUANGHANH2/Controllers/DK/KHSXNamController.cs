0bf04d5f1eb3a26097d549df6a574d82a47b8bb0
Long Vu <vulong22121999@gmail.com>
2019-12-11 12:44:20 +0700

    KHSXNam.cshtml va KHSXNam.cs

---
+++ QUANGHANH2/Controllers/DK/KHSXNamController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using QUANGHANH2.Models;
+namespace QUANGHANH2.Controllers.DK
+{
+    public class KHSXNamController : Controller
+    {
+        // GET: KHSXNam
+        [Route("phong-dieu-khien/ke-hoach-san-xuat-nam")]
+        public ActionResult Index()
+        {
+            QUANGHANHABCEntities dbContext = new QUANGHANHABCEntities();
+            List<TieuChi> listTieuChi = dbContext.Database.SqlQuery<TieuChi>("select * from TieuChi").ToList<TieuChi>();
+            ViewBag.listTC = listTieuChi;
+            return View("/Views/DK/KHSXNam.cshtml");
+        }
+    }
+}
