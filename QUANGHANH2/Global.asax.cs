6d67f168dcf4f5b4770f9aa7007376b88dcb1716
thang1160 <35557579+thang1160@users.noreply.github.com>
2019-12-17 21:13:34 +0700

    log request

--- QUANGHANH2/Global.asax.cs
+++ QUANGHANH2/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QUANGHANH2.Controllers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +13,7 @@ namespace QUANGHANH2
     {
         protected void Application_Start()
         {
+            GlobalFilters.Filters.Add(new LogActionFilter());
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
