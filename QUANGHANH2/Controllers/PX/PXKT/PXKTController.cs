ba780245ac9176ed07b668d6d51468ac7b28426a
thang1160 <35557579+thang1160@users.noreply.github.com>
2020-01-09 19:38:48 +0700

    Sửa màn thông tin đi làm của nhân viên

--- QUANGHANH2/Controllers/PX/PXKT/PXKTController.cs
+++ QUANGHANH2/Controllers/PX/PXKT/PXKTController.cs
@@ -234,6 +234,7 @@ namespace QUANGHANHCORE.Controllers.PX.PXKT
             }
         }
 
+        [Auther(RightID = "179,180,181,182,183,184,185,186,187,188,189")]
         [HttpPost]
         [Route("phan-xuong-khai-thac/diem-danh")]
         public ActionResult takeAttendance()
