5b9c80a9d9499aec0663728c3fefb51360c90888
N0SBa <nguyenbason.fpt04041999@gmail.com>
2020-03-06 17:01:39 +0700

    Update layout, view and controller

---
+++ QUANGHANH2/Controllers/DK/Department_Criteria/Department_Criteria_By_YearController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QUANGHANH2.Controllers.DK.Department_Criteria
+{
+    public class Department_Criteria_By_YearController : Controller
+    {
+        [Route("phong-dieu-khien/nhap-lieu-phong-ban-tieu-chi-theo-nam")]
+        public ActionResult Index()
+        {
+            return View("/Views/DK/Department_Criteria/Department_Criteria_By_Year.cshtml");
+        }
+    }
+}
