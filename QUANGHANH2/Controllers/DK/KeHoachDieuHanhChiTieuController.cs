c71006fe7ae7370e4a80a03e3545a5200e4ce31d
duongpl <51127228+duongpl@users.noreply.github.com>
2020-03-10 15:34:16 +0700

    thêm màn kế hoạch chỉ tiêu theo năm

---
+++ QUANGHANH2/Controllers/DK/KeHoachDieuHanhChiTieuController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QUANGHANH2.Controllers.DK
+{
+    public class KeHoachDieuHanhChiTieuController : Controller
+    {
+        // GET: KeHoachDieuHanhChiTieu
+        [Route("phong-dieu-khien/ke-hoach-dieu-hanh-chi-tieu")]
+        public ActionResult Index()
+        {
+            ViewBag.thisyear = DateTime.Now.Year;
+            return View("/Views/DK/KeHoachDieuHanhChiTieu.cshtml");
+        }
+        [Route("phong-dieu-khien/ke-hoach-dieu-hanh-chi-tieu/lay-du-lieu")]
+        public ActionResult GetData(int year)
+        {
+            return Json("", JsonRequestBehavior.AllowGet);
+        }
+    }
+}
