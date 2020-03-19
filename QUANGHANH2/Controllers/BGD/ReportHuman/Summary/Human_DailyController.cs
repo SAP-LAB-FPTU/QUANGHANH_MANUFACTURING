b283e470b4ddaf211ae1dd7e6fdc152833bbf5fc
N0SBa <nguyenbason.fpt04041999@gmail.com>
2020-03-19 14:18:09 +0700

    Update new monitor

--- QUANGHANH2/Controllers/BGD/ReportHuman/Summary/Human_DailyController.cs
+++ QUANGHANH2/Controllers/BGD/ReportHuman/Summary/Human_DailyController.cs
@@ -11,13 +11,7 @@ namespace QUANGHANH2.Controllers.DK.ReportHuman
         [Route("phong-dieu-khien/bao-cao-nhan-luc/bao-cao-tong-hop-nhan-luc-theo-ngay")]
         public ActionResult Index()
         {
-            return View("/Views/DK/ReportHuman/Summary/Human_Daily.cshtml");
-        }
-
-        [Route("phong-dieu-khien/bao-cao-nhan-luc/bao-cao-tong-hop-nhan-theo-phan-xuong-theo-ngay")]
-        public ActionResult Index_2()
-        {
-            return View("/Views/DK/ReportHuman/Summary/Human_Department_Daily.cshtml");
+            return View("/Views/BGD/ReportHuman/Summary/Human_Daily.cshtml");
         }
     }
 }
