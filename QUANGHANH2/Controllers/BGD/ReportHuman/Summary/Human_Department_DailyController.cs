c5d422f4210345040f7e8daa55428f48e8b3a0eb
N0SBa <nguyenbason.fpt04041999@gmail.com>
2020-04-10 17:44:28 +0700

    Update human report BGD

--- QUANGHANH2/Controllers/BGD/ReportHuman/Summary/Human_Department_DailyController.cs
+++ QUANGHANH2/Controllers/BGD/ReportHuman/Summary/Human_Department_DailyController.cs
@@ -8,7 +8,7 @@ namespace QUANGHANH2.Controllers.BGD.ReportHuman.Summary
 {
     public class Human_Department_DailyController : Controller
     {
-        [Route("phong-dieu-khien/bao-cao-nhan-luc/bao-cao-tong-hop-nhan-theo-phan-xuong-theo-ngay")]
+        [Route("ban-giam-doc/bao-cao-nhan-luc/bao-cao-tong-hop-nhan-theo-phan-xuong-theo-ngay")]
         public ActionResult Index()
         {
             return View("/Views/BGD/ReportHuman/Summary/Human_Department_Daily.cshtml");
