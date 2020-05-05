46e8b92d833bed36a48694c1d245fb72399ad1a3
thang1160 <35557579+thang1160@users.noreply.github.com>
2020-05-06 02:04:57 +0700

    tạo quyết định

--- QUANGHANH2/Controllers/CDVT/Quyetdinh/SuaChua/ChiTietController.cs
+++ QUANGHANH2/Controllers/CDVT/Quyetdinh/SuaChua/ChiTietController.cs
@@ -1,4 +1,6 @@
-﻿using QUANGHANH2.Models;
+﻿using Newtonsoft.Json.Linq;
+using QUANGHANH2.Models;
+using QUANGHANH2.SupportClass;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,55 +14,99 @@ namespace QUANGHANH2.Controllers.CDVT.Quyetdinh.SuaChua
 {
     public class ChiTietController : Controller
     {
-        [HttpGet]
+        [Auther(RightID = "30")]
         [Route("phong-cdvt/quyet-dinh/sua-chua/chi-tiet")]
         public ActionResult LoadPage(int id)
         {
             using (QUANGHANHABCEntities db = new QUANGHANHABCEntities())
             {
                 db.Configuration.LazyLoadingEnabled = false;
-                List<Detail> details = new List<Detail>();
                 List<string> equipmentId = new List<string>();
 
-                List<Detail> temp = (from a in db.Documentary_repair_details
-                                     join b in db.Documentaries on a.documentary_id equals b.documentary_id
-                                     where (b.documentary_id == id)
-                                     select new Detail
-                                     {
-                                         equipmentId = a.equipmentId,
-                                         equipmentId_dikem = a.equipmentId_dikem,
-                                         repair_reason = a.repair_reason,
-                                         repair_type = a.repair_type,
-                                         finish_date_plan = a.finish_date_plan,
-                                         quantity = a.quantity
-                                     }).ToList();
-                foreach (Detail item in temp)
+                List<Detail> details = (from a in db.Documentary_repair_details
+                                        join b in db.Documentaries on a.documentary_id equals b.documentary_id
+                                        where (b.documentary_id == id)
+                                        select new Detail
+                                        {
+                                            documentary_repair_id = a.documentary_repair_id,
+                                            equipmentId = a.equipmentId_dikem == null ? a.equipmentId : (a.equipmentId_dikem + "  (" + a.equipmentId + ")"),
+                                            equipmentId_dikem = a.equipmentId_dikem,
+                                            repair_reason = a.repair_reason,
+                                            repair_type = a.repair_type,
+                                            finish_date_plan = a.finish_date_plan,
+                                            quantity = a.quantity
+                                        }).ToList();
+
+                foreach (Detail item in details)
                 {
                     if (item.equipmentId_dikem == null)
                     {
                         equipmentId.Add(item.equipmentId);
-                        details.Add(item);
                     }
                     else
                     {
                         equipmentId.Add(item.equipmentId_dikem);
-                        details.Add(new Detail
-                        {
-                            equipmentId = item.equipmentId_dikem + " (" + item.equipmentId + ")",
-                            repair_reason = item.repair_reason,
-                            repair_type = item.repair_type,
-                            finish_date_plan = item.finish_date_plan,
-                            quantity = item.quantity
-                        });
                     }
                 }
+
+                var dict = db.Equipments
+                    .Where(x => equipmentId.Contains(x.equipmentId))
+                    .Select(x => new { x.equipmentId, x.equipment_name })
+                    .AsEnumerable()
+                    .ToDictionary(x => x.equipmentId, x => x.equipment_name);
+
+                details.ForEach(x => x.equipment_name = x.equipmentId_dikem == null ? dict[x.equipmentId] : dict[x.equipmentId_dikem]);
                 ViewBag.details = details;
             }
             return View("/Views/CDVT/Quyetdinh/SuaChua/ChiTiet.cshtml");
         }
 
+        [Auther(RightID = "30")]
+        [HttpPost]
+        [Route("phong-cdvt/quyet-dinh/sua-chua/chi-tiet")]
+        public ActionResult GetDetail(int documentary_repair_id, bool isSupply)
+        {
+            try
+            {
+                using (QUANGHANHABCEntities db = new QUANGHANHABCEntities())
+                {
+                    if (isSupply)
+                    {
+                        var list = (from a in db.Supplies
+                                    join b in db.Supply_Documentary_Repair_Equipment on a.supply_id equals b.supply_id
+                                    where b.documentary_repair_id == documentary_repair_id
+                                    select new
+                                    {
+                                        a.supply_id,
+                                        a.supply_name,
+                                        b.quantity_plan
+                                    }).ToList();
+                        return Json(new { success = true, data = list });
+                    }
+                    else
+                    {
+                        var list = (from a in db.Equipments
+                                    join b in db.Supply_Documentary_Repair_Equipment on a.equipmentId equals b.equipmentId
+                                    where b.documentary_repair_id == documentary_repair_id
+                                    select new
+                                    {
+                                        a.equipmentId,
+                                        a.equipment_name,
+                                        b.quantity_plan
+                                    }).ToList();
+                        return Json(new { success = true, data = list });
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                return Json(new { success = false });
+            }
+        }
+
         public class Detail
         {
+            public int documentary_repair_id { get; set; }
             public string equipmentId { get; set; }
             public string equipmentId_dikem { get; set; }
             public string equipment_name { get; set; }
