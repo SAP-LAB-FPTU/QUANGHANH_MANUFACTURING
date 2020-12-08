4f0401192a42a18d4b2d2c3da67df92073e52acf
thang1160 <35557579+thang1160@users.noreply.github.com>
2020-12-08 14:44:15 +0700

    Model - [update] Fix connection strings

--- QUANGHANH2/Models/QuangHanhManufacturingModel.Designer.cs
+++ QUANGHANH2/Models/QuangHanhManufacturingModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'E:\MajorTermIX\Capstone\Project\QUANGHANH_MANUFACTURING\QUANGHANH2\Models\QuangHanhManufacturingModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\SAPLAB-DESKTOP\Documents\GitHub\QUANGHANH_MANUFACTURING\QUANGHANH2\Models\QuangHanhManufacturingModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
