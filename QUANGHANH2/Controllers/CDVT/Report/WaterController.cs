45305ef155126327c59afd48e6a8c35ad0141125
duongpl <51127228+duongpl@users.noreply.github.com>
2020-12-08 13:20:57 +0700

    sum

--- QUANGHANH2/Controllers/CDVT/Report/WaterController.cs
+++ QUANGHANH2/Controllers/CDVT/Report/WaterController.cs
@@ -22,6 +22,7 @@ namespace QUANGHANHCORE.Controllers.CDVT.Report
         {
             using (QuangHanhManufacturingEntities db = new QuangHanhManufacturingEntities())
             {
+                if(type == null) date = DateTime.Now.Date.ToString("dd/MM/yyyy");
                 List<GetWaterReport_Result> listdata = db.Database.SqlQuery<GetWaterReport_Result>("Equipment.WaterReport {0}, {1}, {2}, {3}, {4}", type, date, month, quarter, year).ToList();
                 if (listdata != null)
                 {
@@ -47,6 +48,7 @@ namespace QUANGHANHCORE.Controllers.CDVT.Report
         {
             using (QuangHanhManufacturingEntities db = new QuangHanhManufacturingEntities())
             {
+                if (type == null) date = DateTime.Now.Date.ToString("dd/MM/yyyy");
                 List<GetWaterReport_Result> listdata = db.Database.SqlQuery<GetWaterReport_Result>("Equipment.WaterReport {0}, {1}, {2}, {3}, {4}", type, date, month, quarter, year).ToList();
                 foreach (var item in listdata)
                 {
@@ -135,6 +137,7 @@ namespace QUANGHANHCORE.Controllers.CDVT.Report
             }
             return query;
         }
+
         [Route("phong-cdvt/bao-cao/thoat-nuoc/excel")]
         public ActionResult Export(string type, string date, string month, string quarter, string year)
         {
@@ -147,6 +150,7 @@ namespace QUANGHANHCORE.Controllers.CDVT.Report
                 ExcelWorksheet excelWorksheet = excelWorkbook.Worksheets.First();
                 using (QuangHanhManufacturingEntities db = new QuangHanhManufacturingEntities())
                 {
+                    if (type == null) date = DateTime.Now.Date.ToString("dd/MM/yyyy");
                     List<GetWaterReport_Result> content = db.Database.SqlQuery<GetWaterReport_Result>("Equipment.WaterReport {0}, {1}, {2}, {3}, {4}", type, date, month, quarter, year).ToList();
                     double totaltieuhao = 0; double totalsanluong = 0; double totalgio = 0;
                     if (content != null)
