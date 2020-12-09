e714de4ff775a2f055ed03a14f37d2e660eb99c5
sonnh <yggdrasil2769@gmail.com>
2020-12-09 15:37:35 +0700

    TCLD - [fix][add] WorkHistoryController, WorkHistory view, add EntityResult GetWorkHistory_Result

---
+++ QUANGHANH2/EntityResult/GetWorkHistory_Result.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QUANGHANH2.EntityResult
+{
+    public class GetWorkHistory_Result
+    {
+        public Nullable<DateTime> attendance_date { get; set; }
+        public string shifts { get; set; }
+        public string note { get; set; }
+    }
+}
