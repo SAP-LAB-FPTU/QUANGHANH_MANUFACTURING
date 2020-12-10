e23627f135bfda634b80860d91c543bb5ddc7179
sonnh <yggdrasil2769@gmail.com>
2020-12-10 15:14:14 +0700

    TCLD - [fix][add] TranferHistoryController fix following new DB, fix check error to alert in WorkHistory, List view

---
+++ QUANGHANH2/EntityResult/GetTranferHistory_Result.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QUANGHANH2.EntityResult
+{
+    public class GetTranferHistory_Result
+    {
+        public string BASIC_INFO_full_name { get; set; }
+        public int decision_id { get; set; }
+        public string number { get; set; }
+        public Nullable<System.DateTime> date { get; set; }
+        public string new_department { get; set; }
+        public string old_department { get; set; }
+        public string new_work { get; set; }
+        public string old_work { get; set; }
+    }
+}
