65da2cb37a80bba10ebdcf20b04e606539af1f92
Quoc Tuan <39939107+quoctuanck99@users.noreply.github.com>
2020-12-17 16:11:08 +0700

    TCLD - [fix] Step 2 on Trasfer Task

---
+++ QUANGHANH2/EntityResult/Transfer_GetCurrentEmployeeInformation_Result.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QUANGHANH2.EntityResult
+{
+    public class Transfer_GetCurrentEmployeeInformation_Result
+    {
+        public string employee_id { get; set; }
+        public string BASIC_INFO_full_name { get; set; }
+        public string department_name { get; set; }
+        public string TenCongViec { get; set; }
+        public double? PhuCap { get; set; }
+        public string BacLuong { get; set; }
+        public string ThangLuong { get; set; }
+        public string Luong { get; set; }
+        public string current_department_id { get; set; }
+        public Nullable<int> current_work_id { get; set; }
+    }
+}
