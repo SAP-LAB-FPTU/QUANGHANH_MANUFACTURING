1a31b78228268d0d23794c6c37aed6f8b1444d71
hvu0163 <53392914+hvu0163@users.noreply.github.com>
2020-12-23 19:52:33 +0700

    CDVT - [fix][add]
    fix DanhsachvattuController, Danhsachvattu
    add GetSuggestSearchSupply_Result

---
+++ QUANGHANH2/EntityResult/GetSuggestSearchSupply_Result.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QUANGHANH2.EntityResult
+{
+    public class GetSuggestSearchSupply_Result
+    {
+        public string name { get; set; }
+    }
+}
